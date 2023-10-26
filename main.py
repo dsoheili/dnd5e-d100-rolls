4e44de56a10df7c88e9a241bd2f7f0ece61c2b86
Daniel Soheili <dansoheili@gmail.com>
2023-10-25 18:05:27 -0700

    Update UI, add item tables

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 import tkinter as tk
 import random
-import re
+from ttkwidgets.autocomplete import AutocompleteCombobox
+from tkinter import ttk
 
 from tables.tables_index import tables
 
-def get_item_from_table(table_name, item_index):
+def get_item_from_table(table_name, item_index, num_items):
     if table_name in tables:
         items = list(tables[table_name])
 
-        if item_index == "":
+        if num_items > 1:
+                result = random.sample(items, num_items)
+        elif item_index == "":
             result = random.choice(items)
         else:
             result = items[int(item_index)-1]
 
         return result
 
-def on_random_click():
-    random_number = random.randint(1, 100)
-    input_entry.delete(0, tk.END)
-    input_entry.insert(0, random_number)
-
-def on_roll_click():
+def on_roll_click(num_items):
     table_name = table_var.get()
     if not table_name:
         result_text.config(state=tk.NORMAL)
@@ -29,10 +27,16 @@ def on_roll_click():
         result_text.config(state=tk.DISABLED)
         return
 
-    result = get_item_from_table(table_name, input_entry.get())
+    result = get_item_from_table(table_name, input_entry.get(), num_items)
     result_text.config(state=tk.NORMAL)
     result_text.delete(1.0, tk.END)
-    result_text.insert(tk.END, result)
+
+    if isinstance(result, list):
+        for i, item in enumerate(result, 1):
+            result_text.insert(tk.END, f"{item}\n")
+    else:
+        result_text.insert(tk.END, result)
+
     result_text.config(state=tk.DISABLED)
 
 app = tk.Tk()
@@ -42,25 +46,28 @@ app.title("D100 Table Roller")
 input_label = tk.Label(app, text="Enter a number (1-100):")
 input_label.pack()
 input_entry = tk.Entry(app)
-input_entry.pack()
-
-# Random button
-random_button = tk.Button(app, text="Random", command=on_random_click)
-random_button.pack()
+input_entry.pack(pady=10)
 
 # Table selector
 table_label = tk.Label(app, text="Select a d100 table:")
 table_label.pack()
 table_var = tk.StringVar()
-table_selector = tk.OptionMenu(app, table_var, *tables.keys())
-table_selector.pack()
+table_selector = AutocompleteCombobox(app, textvariable=table_var, completevalues=list(tables.keys()))
+table_selector.pack(pady=10)
+table_selector.config(width=30)
 
-# Roll button
-roll_button = tk.Button(app, text="Roll", command=on_roll_click)
-roll_button.pack()
+# Roll buttons
+button_frame = tk.Frame(app)
+button_frame.pack()
+
+roll_button = tk.Button(button_frame, text="Roll", command=lambda: on_roll_click(1))
+roll_button.pack(side=tk.LEFT, padx=10)
+
+roll_button_10 = tk.Button(button_frame, text="Roll 10", command=lambda: on_roll_click(10))
+roll_button_10.pack(side=tk.LEFT, padx=10, pady=10)
 
 # Output field
-result_text = tk.Text(app, wrap=tk.WORD, height=10, width=40)
+result_text = tk.Text(app, wrap=tk.WORD, height=12, width=40)
 result_text.pack()
 result_text.config(state=tk.DISABLED)
 
--- tables/tables_index.py
+++ tables/tables_index.py
@@ -1,9 +1,37 @@
 # Import all external variables
-from tables.items.wizards_chamber import wizard_chamber_items
-from tables.items.alchemists_lab import alchemists_lab_items
+from .items.wizards_chamber import wizard_chamber_items
+from .items.alchemists_lab import alchemists_lab_items
+from .items.cottage import cottage_items
+from .items.bandit_hideout import bandit_hideout_items
+from .items.office import office_items
+from .items.warehouse import warehouse_items
+from .items.royal_tomb import royal_tomb_items
+from .items.nobles_bedchamber import nobles_bechamber_items
+from .items.port_masters_office import port_masters_office_items
+from .items.adventurers_dead_body import adventurers_dead_body_items
+from .items.hunters_camp import hunters_camp_items
+from .items.ship_captains_quarters import ship_captains_quarters_items
+from .items.dead_goblin import dead_goblin_items
+from .items.fantasy_desk import fantasy_desk_items
+from .items.inns_kitchen import inns_kitchen_items
+from .items.weapons_armor_and_equipment import weapons_armor_and_equipment_items
 
 # Index for all tables
 tables = {
     "Wizard's Chamber": wizard_chamber_items,
-    "Alchemist's Lab": alchemists_lab_items
+    "Alchemist's Lab": alchemists_lab_items,
+    "Cottage": cottage_items,
+    "Bandit Hideout": bandit_hideout_items,
+    "Office": office_items,
+    "Warehouse": warehouse_items,
+    "Royal Tomb": royal_tomb_items,
+    "Noble's Bedchamber": nobles_bechamber_items,
+    "Port Master's Office": port_masters_office_items,
+    "Adventurer's Dead Body": adventurers_dead_body_items,
+    "Hunter's Camp": hunters_camp_items,
+    "Ship Captain's Quarters": ship_captains_quarters_items,
+    "Dead Goblin": dead_goblin_items,
+    "Fantasy Desk": fantasy_desk_items,
+    "Inn's Kitchen": inns_kitchen_items,
+    "Weapons, Armor and Equipment": weapons_armor_and_equipment_items
 }
